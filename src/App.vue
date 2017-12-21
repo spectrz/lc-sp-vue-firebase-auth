814f235abbe2f203a4519d84c70a84650464c388
name <email>
2017-12-21 00:14:11 -0700

    app.vue now uses pug, added nav to top with login/out functionality

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
-<template>
+<!-- <template>
   <div id="app" class="container">
+  
+
     <router-link to="/sign-up">Sign Up</router-link>
     <router-link to="/login">Log In</router-link>
     <router-link to="/hello">Hello</router-link>
@@ -9,12 +11,80 @@
     <img src="./assets/logo.png">
     <router-view/>
   </div>
+</template> -->
+
+<template lang="pug">
+  #app.container
+    .panel.panel-default
+      .row
+        .col-sm-6
+          ul.nav.nav-pills
+            li 
+              
+              router-link(to="/hello") 
+                a Hello
+            li 
+              router-link(to="/todo")
+                a Todo
+            li
+              router-link(to="/about")
+                a About
+        .col-sm-6
+          ul.nav.nav-pills.pull-right(v-show="!isLoggedIn")
+            li 
+              router-link(to="/login")
+                a Login
+            li 
+              router-link(to="/sign-up")
+                a Signup
+          ul.nav.nav-pills.pull-right(v-show="isLoggedIn")
+            li 
+              a(v-on:click="logout") Logout
+
+    img(src="./assets/logo.png")
+    | {{isLoggedIn}}
+    router-view
 </template>
 
 <script>
+import frb from '../firebaseMod'
+
+let userlog;// = firebase.auth().user != null;
+
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+        return {
+            isLoggedIn: userlog
+        }
+    },
+  created() {
+    
+    frb.auth().onAuthStateChanged( (user) => {
+      if ( user ) {
+        this.isLoggedIn = true;
+      } else {
+        this.isLoggedIn = false;
+      }
+    });
+  },
+  methods: {
+    logout: function() {
+      frb.auth().signOut().then( () => {
+        this.$router.replace('login');
+      });
+    }
+  }
 }
+
+// frb.auth().onAuthStateChanged( (user) => {
+//     if ( user ) {
+//         userlog = true;
+//     } else {
+//         userlog = false;
+//     }
+// });
+
 </script>
 
 <style>
